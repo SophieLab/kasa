cd585bd54fc2f08a448cb1a7087de99832ec6a74
Sophie Labyt <labytsophie@gmail.com>
2024-08-19 19:22:54 +0200

    add images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./styles/app.scss";
+import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
+import Home from "./pages/Home/Home";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/accueil" />} />
+        <Route path="/accueil" element={<Home />} />
+      </Routes>
+    </HashRouter>
   );
-}
+};
 
 export default App;
