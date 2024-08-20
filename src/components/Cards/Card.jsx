f432980e15343622d850356fe464b1da12cc6469
Sophie Labyt <labytsophie@gmail.com>
2024-08-20 22:34:33 +0200

    style card

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,10 +1,13 @@
-export default function Card({ cover, title }) { //les cards de locations récupèrent les couvertures et titres en props depuis Home
-	return (
-		<article className="card-logement">
-			<img src={cover} alt="location" />
-			<div className="card-logement__layer">
-				<p className="card-logement__title">{title}</p>
-			</div>
-		</article>
-	);
-}
+import React from 'react';
+
+// Composant Card : Affiche une carte de location avec une couverture et un titre
+const Card = ({ cover, title }) => (
+    <article className="card-logement">
+        <img src={cover} alt="location" />
+        <div className="card-logement__layer">
+            <p className="card-logement__title">{title}</p>
+        </div>
+    </article>
+);
+
+export default Card;
