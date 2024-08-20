75c25e6bd9e0597824de26f8113e4a41cf7ab6fa
Sophie Labyt <labytsophie@gmail.com>
2024-08-20 22:28:56 +0200

    style Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,11 @@
-export default function Banner() {
-	return (
-		<div className="banner-container">
-			<div className="banner-text-container">
-				<div className="banner-text">Chez vous, partout et ailleurs</div>
-			</div>
-		</div>
-	);
-}
+import React from 'react';
+
+const Banner = () => (
+    <div className="banner-container">
+        <div className="banner-text-container">
+            <div className="banner-text">Chez vous, partout et ailleurs</div>
+        </div>
+    </div>
+);
+
+export default Banner;
